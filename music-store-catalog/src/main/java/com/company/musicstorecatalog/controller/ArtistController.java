aea29aa89c982666a88e7a8447a0219f4c2540c7
paperpatch <patchen21@gmail.com>
2022-08-03 12:15:19 -0400

    added exceptions handler controller, custom error messages, and NotFoundException

--- music-store-catalog/src/main/java/com/company/musicstorecatalog/controller/ArtistController.java
+++ music-store-catalog/src/main/java/com/company/musicstorecatalog/controller/ArtistController.java
@@ -1,8 +1,16 @@
 package com.company.musicstorecatalog.controller;
 
+import com.company.musicstorecatalog.exceptions.NotFoundException;
+import com.company.musicstorecatalog.model.Artist;
 import com.company.musicstorecatalog.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ArtistController {
@@ -10,5 +18,42 @@ public class ArtistController {
     @Autowired
     ServiceLayer service;
 
+    @GetMapping("/artist")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Artist> getAllArtists() {
+        return service.getAllArtists();
+    }
 
+    @GetMapping("/artist/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Artist> getArtistById(@PathVariable int id) {
+        if (service.getArtistById(id).orElse(null) == null) {
+            throw new NotFoundException("No artist with that ID exists");
+        }
+        return service.getArtistById(id);
+    }
+
+    @PostMapping("/artist")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Artist createArtist(@Valid @RequestBody Artist artist) {
+        return service.createArtist(artist);
+    }
+
+    @PutMapping("/artist/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateArtist(@PathVariable int id, @Valid @RequestBody Artist artist) {
+        if (id != artist.getArtistId()) {
+            throw new DataIntegrityViolationException("Your request body ID does not match your Path Variable ID");
+        }
+        service.updateArtist(artist);
+    }
+
+    @DeleteMapping("/artist/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteArtist(@PathVariable int id) {
+        if (service.getArtistById(id).orElse(null) == null) {
+            throw new NotFoundException("No artist with that ID exists");
+        }
+        service.deleteArtist(id);
+    }
 }
---
+++ music-store-catalog/src/main/java/com/company/musicstorecatalog/model/CustomErrorResponse.java
@@ -0,0 +1,77 @@
+package com.company.musicstorecatalog.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class CustomErrorResponse {
+    private String errorMsg;
+    private int status;
+    private String errorCode;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+    private LocalDateTime timestamp;
+
+    public CustomErrorResponse() {
+    }
+
+    public CustomErrorResponse(String errorCode, String errorMsg) {
+        this.errorCode = errorCode;
+        this.errorMsg = errorMsg;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CustomErrorResponse that = (CustomErrorResponse) o;
+        return status == that.status && Objects.equals(errorMsg, that.errorMsg) && Objects.equals(errorCode, that.errorCode) && Objects.equals(timestamp, that.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(errorMsg, status, errorCode, timestamp);
+    }
+
+    @Override
+    public String toString() {
+        return "CustomErrorResponse{" +
+                "errorMsg='" + errorMsg + '\'' +
+                ", status=" + status +
+                ", errorCode='" + errorCode + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
