fbd6d85dfaf710252211d31095be9f5143cdfeea
paperpatch <patchen21@gmail.com>
2022-08-04 12:45:32 -0400

    added album recommedation controller tests

--- music-store-recommendations/src/test/java/com/company/musicstorerecommendations/controller/AlbumRecommendationControllerTest.java
+++ music-store-recommendations/src/test/java/com/company/musicstorerecommendations/controller/AlbumRecommendationControllerTest.java
@@ -9,15 +9,17 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -77,6 +79,69 @@ public class AlbumRecommendationControllerTest {
     @Test
     public void shouldReturnStatus404WhenAlbumRecommendationIdNotAvailable() throws Exception {
         mockMvc.perform(get("/albumRecommendation/12345"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldCreateAlbumRecommendationAndStatus201() throws Exception {
+        expectedJson = mapper.writeValueAsString(expectedAlbumRecommendation);
+        inputJson = mapper.writeValueAsString(inputAlbumRecommendation);
+
+        mockMvc.perform(post("/albumRecommendation")
+                .content(inputJson)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(content().json(expectedJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void shouldReturn400WithBadAlbumRecommendationPostRequest() throws Exception {
+        HashMap<String, Object> invalid = new HashMap();
+        invalid.put("title", "nothing");
+        invalid.put("albumId", "string");
+        inputJson = mapper.writeValueAsString(invalid);
+
+        mockMvc.perform(post("/albumRecommendation")
+                .content(inputJson)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldUpdateAlbumRecommendationWithStatus204() throws Exception {
+        inputJson = mapper.writeValueAsString(inputAlbumRecommendation);
+
+        mockMvc.perform(put("/albumRecommendation/1")
+                .content(inputJson)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void shouldReturn422IfPutAlbumRecommendationIdNotExist() throws Exception {
+        inputJson = mapper.writeValueAsString(inputAlbumRecommendation);
+
+        mockMvc.perform(put("/albumRecommendation/12345")
+                        .content(inputJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void shouldDeleteAlbumRecommendationWithStatus204() throws Exception {
+        mockMvc.perform(delete("/albumRecommendation/1"))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void shouldReturn404IfAlbumRecommendationIsNotFound() throws Exception {
+        mockMvc.perform(delete("/albumRecommendation/12345"))
                 .andDo(print())
                 .andExpect(status().isNotFound());
     }
